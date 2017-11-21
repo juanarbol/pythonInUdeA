0e90ce94841297ba9cf85ac410f95ad7e851e465
Juan José <juan.jose.arboleda.a@gmail.com>
2017-11-20 19:39:29 -0500

    Hacer la aclaración del tamaño de la placa y posicionamiento de la fuente

--- heat-transfer/heat_transfer.py
+++ heat-transfer/heat_transfer.py
@@ -27,19 +27,19 @@ Q = mc(T2-T1)
 fig, ax = plt.subplots() # plot declaration
 
 ###### Prompt values
-matrix_x_axis = input('Digite el tamaño de la placa con respecto al eje x: ') # Size of board in x axis
+matrix_x_axis = input('Digite el tamaño de la placa con respecto al eje x (en metros): ') # Size of board in x axis
 matrix_x_axis = int(matrix_x_axis)
 
-matrix_y_axis = input('Digite el tamaño de la placa con respecto al eje y: ') # Size of board in y axis
+matrix_y_axis = input('Digite el tamaño de la placa con respecto al eje y (en metros): ') # Size of board in y axis
 matrix_y_axis = int(matrix_y_axis)
 
 font_temperature_input = int(input('Digite la temperatura de la fuente (Cº): '))
 font_temperature = font_temperature_input if font_temperature_input > 30 else 35
 
-font_x_position_input = int(input('Digite la posición de la fuente (x): '))
+font_x_position_input = int(input('Digite la posición de la fuente (x cm\'s): '))
 font_x_position = font_x_position_input if font_x_position_input < matrix_x_axis else int(matrix_x_axis/2)
 
-font_y_position_input = int(input('Digite la posición de la fuente (y): '))
+font_y_position_input = int(input('Digite la posición de la fuente (y cm\'s): '))
 font_y_position = font_y_position_input if font_y_position_input < matrix_y_axis else int(matrix_y_axis/2)
 
 ###### constants
