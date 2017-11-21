f85eebac9a8e93b97591da6c120f9e1d3c64f66e
Juan José <juan.jose.arboleda.a@gmail.com>
2017-11-20 20:39:50 -0500

    Cambios del nombre del parámetro, nada especial

--- heat-transfer/graph.py
+++ heat-transfer/graph.py
@@ -1,7 +1,7 @@
 from heat_transfer_loop import heat_transfer_loop
 import matplotlib.pyplot as plt
 
-def graph(heat_transfer_plot, temperature_matrix, constants, current_temperature):
+def graph(heat_transfer_plot, temperature_matrix, constants, current_time):
   font_temperature = constants['font_temperature']
   font_x_position = constants["font_x_position"]
   font_y_position = constants["font_y_position"]
@@ -9,7 +9,7 @@ def graph(heat_transfer_plot, temperature_matrix, constants, current_temperature
   new_temperature_matrix = heat_transfer_loop(temperature_matrix, constants).copy()
   new_temperature_matrix[font_x_position, font_y_position] = font_temperature # Set the font position ?? [x,y]
   heat_transfer_plot.set_data(new_temperature_matrix)
-  plt.title("%d Cº" %(current_temperature))
+  plt.title("dT: %d" %(current_time))
   plt.pause(0.0007)
 
   return new_temperature_matrix
